7431e6c1fcbc41c38af86d679bd6bcf1bf0664bf
ashborn3 <114130969+ashborn3@users.noreply.github.com>
2024-04-15 12:06:43 +0530

    Implemented ngrep

--- initial-utilities/ngrep.c
+++ initial-utilities/ngrep.c
@@ -1,12 +1,28 @@
 #include <stdio.h>
+#include <sys/types.h>
+#include <string.h>
+#include <stdlib.h>
 
 #define SIZE 1000
 
 int main(int argc, char* argv[]) {
-    FILE* fp;
-    char* buffer[SIZE];
-    for (int i = 2; i < argc; i++) {
+    FILE *fp = NULL;
+    char *str = NULL;
+    char *line = malloc(SIZE);
+    size_t n = SIZE;
+    for(int i = 2; i < argc; i++) {
         fp = fopen(argv[i], "r");
+        printf("%s\n", argv[i]);
         if (fp == NULL) {
-            printf("ngrep: failed to open file %s\n", argv[i]);
+            printf("Error: File not found\n");
+            return 1;
         }
+        while (getline(&line, &n, fp) > 0) {
+            str = strstr(line, argv[1]);
+            if (str != NULL) printf("%s\n", line);
+        }
+        fclose(fp);
+    }
+    free(line);
+    return 0;
+}
