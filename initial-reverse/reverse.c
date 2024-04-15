deb0206658ad40bbe4c1176946abfd43514b4cbf
ashborn3 <114130969+ashborn3@users.noreply.github.com>
2024-04-15 17:44:54 +0530

    Updated reverse

--- initial-reverse/reverse.c
+++ initial-reverse/reverse.c
@@ -5,11 +5,34 @@
 #define SIZE 1000
 
 int main(int argc, char* argv[]) {
+    if (argc == 1) {
+        printf("No Argument Given\nreverse <INPUT> <OUTPUT>\n");
+        exit(1);
+    }
+    else if (argc == 3) {
+        if (argv[1] == argv[2]) {
+            printf("Input and Output Files can't be the same\n");
+            exit(1);
+        }
+    }
+    else if (argc > 3) {
+        printf("reverse <INPUT> <OUTPUT>\n");
+        exit(1);
+    }
+    
+
+
     char* line;
     char* lineArr[SIZE];
     size_t n;
     int indexOfLineArr = 0;
     FILE *fp = fopen(argv[1], "r");
+
+    if (fp == NULL) {
+        printf("Input file failed to open\n");
+        exit(1);
+    }
+
     while (getline(&line, &n, fp) != -1) {
         char* allocatedLine = (char *) malloc(SIZE);
         strncpy(allocatedLine, line, SIZE);
@@ -24,6 +47,12 @@ int main(int argc, char* argv[]) {
     }
     else if (argc == 3) {
         FILE *fp2 = fopen(argv[2], "w");
+
+        if (fp2 == NULL) {
+            printf("Output file failed to open\n");
+            exit(1);
+        }
+
         for (int i = indexOfLineArr - 1; i >= 0; i--) {
             fprintf(fp2, "%s", lineArr[i]);
             free(lineArr[i]);
